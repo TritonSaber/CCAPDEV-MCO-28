c2aac1fa476744a709560c190d9117c6fa960e32
Ktg8 <113493429+Ktg8@users.noreply.github.com>
2022-11-09 08:34:39 +0800

    Create reservationModel.js
    Signed-off-by: Ktg8 <113493429+Ktg8@users.noreply.github.com>

---
+++ mco2/models/reservationModel.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose"); 
+const DateOnly = require('mongoose-dateonly')(mongoose);
+
+var reservation = new mongoose.Schema({
+    restaurant: {
+        type: String,
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+
+    username: {
+        type: String,
+        required: true
+    },
+
+    datein:{
+        type: DateOnly,
+        required: true
+    },
+
+    timein:{
+        type: String,
+        required: true
+    },
+
+    numpeople:{
+        type: String,
+        required: true
+    },
+
+    card:{
+        type: String,
+        required: true
+    },
+
+    cardnum:{
+        type: Number,
+        required: true
+    },
+
+    cvv:{
+        type: Number,
+        required: true
+    },
+
+    monthexp:{
+        type: String,
+        required: true
+    },
+});
+
+var reserve = mongoose.model("reserves", reservation);
+
+module.exports =  reserve;
