5598d183a9e826081f6ce5f634ec9ccae3a01432
Justine Joshua Tan <justinejoshuatan@yahoo.com>
2022-11-25 18:19:46 +0800

    route update

--- mco2/routes/routes.js
+++ mco2/routes/routes.js
@@ -101,7 +101,7 @@ route.post('/postnewsletter', controller.postNewsletter);
 
 route.get('/geteditprof', controller.getEdit);
 
-route.post('/updateProfile', controller.postProfile);
+route.post('/updateprofile', upload.single('image'), controller.postProfile);
 
 
 module.exports =  route;
